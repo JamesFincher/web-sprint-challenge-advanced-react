22d54ad39b3b2dcfcb904efce12cd13214251370
heartandkarma <heartandkarma@gmail.com>
2022-04-14 23:09:29 -0700

    65/111 class should be done

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 const initialState = {
   x: 2,
@@ -16,7 +17,9 @@ const initialState = {
     [2, 3, false, null],
     [3, 3, false, null],
   ],
+  message: "",
 };
+
 export default class AppClass extends React.Component {
   constructor() {
     super();
@@ -24,35 +27,157 @@ export default class AppClass extends React.Component {
   }
   render() {
     const { className } = this.props;
+    const { x, y, steps, email, message, grid } = this.state;
+    const moveLeft = () => {
+      if (x > 1) {
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          x: this.state.x - 1,
+        });
+      } else {
+        this.setState({
+          message: "You can't go left",
+        });
+      }
+    };
+    const moveRight = () => {
+      if (x >= 1 && x < 3) {
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          x: this.state.x + 1,
+        });
+      } else {
+        this.setState({
+          message: "You can't go right",
+        });
+      }
+    };
+    const moveUp = () => {
+      if (y > 1) {
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          y: this.state.y - 1,
+        });
+      } else {
+        this.setState({
+          message: "You can't go up",
+        });
+      }
+    };
+    const moveDown = () => {
+      if (y >= 1 && y < 3) {
+        this.setState({
+          ...this.state,
+          steps: this.state.steps + 1,
+          y: this.state.y + 1,
+        });
+      } else {
+        this.setState({
+          message: "You can't go down",
+        });
+      }
+    };
+    const reset = () => {
+      this.setState(initialState);
+    };
+    const setLocation = () => {
+      grid.map((group) => {
+        if (group[0] === x && group[1] === y) {
+          group[2] = true;
+          group[3] = "B";
+        } else {
+          group[2] = false;
+          group[3] = null;
+        }
+      });
+    };
+    setLocation();
+    const onSubmit = (event) => {
+      event.preventDefault();
+      const payload = { x, y, steps, email };
+      this.setState({
+        ...this.state,
+        message: "",
+      });
+      axios
+        .post("http://localhost:9000/api/result", payload)
+        .then((res) => {
+          console.log(res);
+          reset();
+          this.setState({
+            ...this.state,
+            message: res.data.message,
+          });
+        })
+        .catch((err) => {
+          this.setState({ ...this.state, message: err.response.data.message });
+        });
+      this.setState({
+        email: "",
+      });
+    };
+    const onChange = (event) => {
+      const { value } = event.target;
+      this.setState({ ...this.state, email: value });
+    };
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">
+            Coordinates ({x}, {y})
+          </h3>
+          <h3 id="steps">
+            You moved {steps} {steps === 1 ? "time" : "times"}
+          </h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          {grid.map((group, idx) => {
+            if (group[2] === true) {
+              return (
+                <div className="square active" key={idx}>
+                  {group[3]}
+                </div>
+              );
+            } else {
+              return (
+                <div className="square" key={idx}>
+                  {group[3]}
+                </div>
+              );
+            }
+          })}
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={moveLeft}>
+            LEFT
+          </button>
+          <button id="up" onClick={moveUp}>
+            UP
+          </button>
+          <button id="right" onClick={moveRight}>
+            RIGHT
+          </button>
+          <button id="down" onClick={moveDown}>
+            DOWN
+          </button>
+          <button id="reset" onClick={reset}>
+            reset
+          </button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={onSubmit}>
+          <input
+            id="email"
+            type="email"
+            placeholder="type email"
+            onChange={onChange}
+            value={this.state.email}
+          ></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
