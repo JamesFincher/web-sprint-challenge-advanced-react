5f69b92f435548e18119de7eb1f3c553c58465e9
heartandkarma <heartandkarma@gmail.com>
2022-04-15 09:45:06 -0700

    got state for functional set

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,6 +1,10 @@
 import React from "react";
 import axios from "axios";
 
+//setting a init var to simplify state management
+//x and y are location on the grid, steps keeps track of steps taken
+//email is the email of the user, grid is the grid of the game,
+//and message is for the api messages to be displayed
 const initialState = {
   x: 2,
   y: 2,
@@ -23,11 +27,15 @@ const initialState = {
 export default class AppClass extends React.Component {
   constructor() {
     super();
+    //set init state
     this.state = initialState;
   }
   render() {
+    //destructuring
     const { className } = this.props;
     const { x, y, steps, email, message, grid } = this.state;
+
+    //function to handle the left click event
     const moveLeft = () => {
       if (x > 1) {
         this.setState({
@@ -41,6 +49,8 @@ export default class AppClass extends React.Component {
         });
       }
     };
+
+    //function to handle the right click event
     const moveRight = () => {
       if (x >= 1 && x < 3) {
         this.setState({
@@ -54,6 +64,8 @@ export default class AppClass extends React.Component {
         });
       }
     };
+
+    //function to handle the up click event
     const moveUp = () => {
       if (y > 1) {
         this.setState({
@@ -67,6 +79,8 @@ export default class AppClass extends React.Component {
         });
       }
     };
+
+    //function to handle the down click event
     const moveDown = () => {
       if (y >= 1 && y < 3) {
         this.setState({
@@ -80,9 +94,13 @@ export default class AppClass extends React.Component {
         });
       }
     };
+
+    //function to handle the reset click event
     const reset = () => {
       this.setState(initialState);
     };
+
+    //function that helps with grid traversal
     const setLocation = () => {
       grid.map((group) => {
         if (group[0] === x && group[1] === y) {
@@ -95,6 +113,9 @@ export default class AppClass extends React.Component {
       });
     };
     setLocation();
+
+    //function to handle the submit click event
+    //creates payload for api, and replaces message with success or error
     const onSubmit = (event) => {
       event.preventDefault();
       const payload = { x, y, steps, email };
@@ -119,10 +140,15 @@ export default class AppClass extends React.Component {
         email: "",
       });
     };
+
+    //function to handle the email change event
+    //keeps the current value of the email input in state
     const onChange = (event) => {
       const { value } = event.target;
       this.setState({ ...this.state, email: value });
     };
+
+    //ui for the class-based component
     return (
       <div id="wrapper" className={className}>
         <div className="info">
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+
+const initialState = {
+  x: 2,
+  y: 2,
+  steps: 0,
+  email: "",
+  grid: [
+    [1, 1, false, null],
+    [2, 1, false, null],
+    [3, 1, false, null],
+    [1, 2, false, null],
+    [2, 2, true, "B"],
+    [3, 2, false, null],
+    [1, 3, false, null],
+    [2, 3, false, null],
+    [3, 3, false, null],
+  ],
+  message: "",
+};
 
 export default function AppFunctional(props) {
+  const { className } = props;
+  //set state
+  const [x, setX] = React.useState(initialState.x);
+  const [y, setY] = React.useState(initialState.y);
+  const [steps, setSteps] = React.useState(initialState.steps);
+  const [email, setEmail] = React.useState(initialState.email);
+  const [grid, setGrid] = React.useState(initialState.grid);
+  const [message, setMessage] = React.useState(initialState.message);
+
+  //helpers for the form
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
@@ -33,5 +64,5 @@ export default function AppFunctional(props) {
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
